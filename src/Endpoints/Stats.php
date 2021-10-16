2d088b4c8f690d7f82bb24e8dd6e4478ef932f58
Jack <JackWH@users.noreply.github.com>
2021-10-16 17:42:00 +0100

    Removed PHPDoc @return tags from Stats.php

--- src/Endpoints/Stats.php
+++ src/Endpoints/Stats.php
@@ -8,8 +8,6 @@ trait Stats
      * Totals
      * Get a list of counts for all of Unsplash.
      * @link https://unsplash.com/documentation#totals
-     *
-     * @return MarkSitko\LaravelUnsplash\Endpoints\Stats
      */
     public function totalStats()
     {
@@ -23,8 +21,6 @@ trait Stats
      * Month
      * Get the overall Unsplash stats for the past 30 days.
      * @link https://unsplash.com/documentation#month
-     *
-     * @return MarkSitko\LaravelUnsplash\Endpoints\Stats
      */
     public function monthlyStats()
     {
